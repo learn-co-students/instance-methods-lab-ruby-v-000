9c1649b2d19cb0a1fe5d11d8cd26899432d0d6e0
Emmanuel Owusu <e_owusu@live.com>
2016-05-30 23:11:50 -0400

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,17 +7,4 @@ class Dog
   def sit()
     puts "The Dog is sitting"
   end
-
-end
-
-class Person
-
-  def talk()
-    puts "Hello World!"
-  end
-
-  def walk()
-    puts "The Person is walking"
-  end
-
 end
