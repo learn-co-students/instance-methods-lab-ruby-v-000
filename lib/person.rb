0f22ae5af65240a5afc7d9a3b57fb4ea5811418f
Jacilyn Hayden <jacihayden@gmail.com>
2017-02-28 00:57:04 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+# #### 4. Define a `Person` in `lib/person.rb`
+# Open `lib/person.rb` and add a class definition for a `Person` class.
+# #### 5. Define `#talk` in `Person`
+# Add an instance method `#talk` to your `Person` class in `lib/person.rb` that will puts `"Hello World!"`
+# #### 6. Define `#walk` in `Person`
+# Add an instance method `#walk` to your `Person` class in `lib/person.rb` that will puts `"The Person is walking"`.
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+  def walk
+    puts "The Person is walking"
+  end
+end
