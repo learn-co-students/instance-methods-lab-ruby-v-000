3c368c90506df546be2ab92b6ab72ef2ac4c3b67
Lucas Kisabeth <lukey.mac@outlook.com>
2017-03-15 19:08:25 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,8 @@ class Dog
     puts "Woof!"
   end
 end
-
+#I would have combined these but learn said the lesson was written
+#with the same code so I had to change it :/
 class Dog
   def sit
     puts "The Dog is sitting"
--- lib/person.rb
+++ lib/person.rb
@@ -3,7 +3,8 @@ class Person
     puts "Hello World!"
   end
 end
-
+#I would have combined these but learn said the lesson was written
+#with the same code so I had to change it :/
 class Person
   def walk
     puts "The Person is walking"
