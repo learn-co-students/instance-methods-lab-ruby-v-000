385e8343c67ec5ca5676040acf71e772433eb30e
Marcelo A Carrillo <marceloacarrillo@gmail.com>
2017-03-24 01:59:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog  # Class body
+
+  def bark # Instance Method Definition
+    puts "Woof!"
+  end
+end
+
+class Dog # Class body
+  def sit # Instance Method Definition
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+class Person  # Class body
+
+  def talk # Instance Method Definition
+    puts "Hello World!"
+  end
+end
+
+class Person # Class body
+  def walk # Instance Method Definition
+    puts "The Person is walking"
+  end
+end
