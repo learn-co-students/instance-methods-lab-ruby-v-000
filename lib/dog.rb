29f0b20f57a3a20fa82620f3c7d7c7b4bac26649
Deborah Seow <deborahseow9@gmail.com>
2017-04-25 03:07:21 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog
+  def bark
+    "Woof!"
+  end
+  def sit
+    "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class person
+  def talk
+    "Hello World!"
+  end
+  def walk
+    "The Person is walking"
+  end
+end
