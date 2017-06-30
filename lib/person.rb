d8b0fbc7030b0df1f91b4513e880dcd30e8f7584
Steven Lennis <steven.lennis@icloud.com>
2017-06-30 13:19:01 -0500

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,15 @@
+class Person
+# Instance Method
+  def talk
+    puts "Hello World!"
+  end
+     # Instance Method
+  def walk
+    puts "The Person is walking"
+  end
+end
+
+adele_goldberg = Person.new
+alan_kaylearn = Person.new
+steven_lennis = Person.new
+Potus_Trump = Person.new
