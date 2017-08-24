437a8e76858402b19177cf7250a139d8cd767be2
Natalie Halawani <hala1224@gmail.com>
2017-08-24 20:03:55 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,14 @@
+class Person
+
+  #### 5. Define `#talk` in `Person`
+
+  def talk
+    puts "Hello World!"
+  end
+
+  #### 6. Define `#walk` in `Person`
+
+  def walk
+     puts "The Person is walking"
+   end
+ end
