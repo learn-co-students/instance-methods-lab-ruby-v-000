2d157b5defdb9ab3ea8539cf4d05612563c28750
kvakili813 Vakili <kvakili813@gmail.com>
2017-10-17 19:00:36 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,21 @@
+class Dog
+
+def bark
+  puts "Woof!"
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+end
+
+class Person
+
+def talk
+  puts "Hello World!"
+
+def walk
+  puts "The Person is walking"
+  end
+end
+end
