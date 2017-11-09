f75e77efe13d7f1a44f2c04cd296b9f7e124cf56
Michael St. Marie <michael.landrum.st.marie@gmail.com>
2017-11-09 17:07:27 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,11 @@
+class Dog #defines the class
+
+  def bark #defines an instance method called "bark" within the Dog class
+  puts "Woof!" #Woof!
+  end
+
+  def sit #defines an instance method called 'sit' within the Dog class
+    puts "The Dog is sitting"
+  end
+  
+end
