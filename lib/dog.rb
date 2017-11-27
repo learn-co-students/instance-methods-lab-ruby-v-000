00cc8fbff19283dea15264d0a8fd27bb776c7627
James Ferguson <ncjamesferguson@gmail.com>
2017-11-27 04:41:17 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,11 @@
 class Dog
 
-def bark
-  puts "Woof!"
-end
+  def bark
+    puts "Woof!"
+  end
 
-def sit
-  puts "The Dog is sitting"
-end
+  def sit
+    puts "The Dog is sitting"
+  end
+
 end
