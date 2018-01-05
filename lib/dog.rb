82e325d3aa7cbb5dc776b492f41fff65451f035f
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 20:56:07 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -9,8 +9,9 @@
 #   end
 # end
 
-
 class Dog
+  attr_accessor :name
+  attr_reader :name
 
     def bark
       puts "Woof!"
@@ -19,15 +20,37 @@ class Dog
     def sit
       puts "The Dog is sitting"
     end
-
-  def name=(dogs_name)
-    @this_dogs_name = dogs_name
-  end
-
-  def name
-    @this_dogs_name
-  end
 end
+
+# class Dog
+# 
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
+#
+#   def name
+#     @this_dogs_name
+#   end
+# end
+
+# class Dog
+#
+#     def bark
+#       puts "Woof!"
+#     end
+#
+#     def sit
+#       puts "The Dog is sitting"
+#     end
+#
+#   def name=(dogs_name)
+#     @this_dogs_name = dogs_name
+#   end
+#
+#   def name
+#     @this_dogs_name
+#   end
+# end
 
 lassie = Dog.new
 lassie.name = "Lassie"
