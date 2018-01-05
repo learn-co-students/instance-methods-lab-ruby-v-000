726ce37d7e4cfa4438c5ea54d15da1def2beb590
Julia J-Prog <juliajen.prog@gmail.com>
2018-01-05 06:06:24 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,35 @@
+#
+# class Dog
+#   def bark
+#     puts "Woof!"
+#   end
+#
+#   def sit
+#     puts "The Dog is sitting"
+#   end
+# end
+
 
 class Dog
-  def bark
-    puts "Woof!"
+
+    def bark
+      puts "Woof!"
+    end
+
+    def sit
+      puts "The Dog is sitting"
+    end
+
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
 
-  def sit
-    puts "The Dog is sitting"
+  def name
+    @this_dogs_name
   end
 end
+
+lassie = Dog.new
+lassie.name = "Lassie"
+
+puts lassie.name
