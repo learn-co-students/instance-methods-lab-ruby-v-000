8a8dc727d06e2a3b1c0f3044109da19c0879c15f
Erica Murdock-Waters <ericahpa@gmail.com>
2018-01-13 18:42:39 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+# def Person class
+class Person
+
+  #def #talk instance method that puts "Hello World!"
+  def talk
+    puts "Hello World!"
+  end
+
+  # def #walk instance method that puts "The Person is walking".
+  def walk
+    puts "The Person is walking"
+  end
+end
