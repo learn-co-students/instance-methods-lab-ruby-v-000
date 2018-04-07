4de376c48db0c2784b086e6c6bb16cdd66a1b02c
Kenneth Kutschera <kennethchoi19@gmail.com>
2018-04-07 04:49:54 +0000

    Done.

--- spec/instance_methods_spec.rb
+++ spec/instance_methods_spec.rb
@@ -1,3 +1,7 @@
+require 'spec_helper.rb'
+require_relative '../lib/dog.rb'
+require_relative '../lib/person.rb'
+
 describe 'Dog' do
   it 'is defined within lib/dog.rb' do
     expect(defined?(Dog)).to be_truthy
