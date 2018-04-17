4c1b1e395a132b0ec447c07c8d4d2eaa46e84324
Lauren Perchitti <laurenperchitti@gmail.com>
2018-04-17 17:04:10 +0000

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,12 +1,18 @@
+
 class Person
-  def talk
-    puts "Hello World!"
-  end
-  def walk
-    puts "The Person is walking"
-  end
+
+
+def talk
+  puts "Hello World!"
 end
 
+def walk
+  puts "The Person is walking"
+end
+
+end
+
+
 ada = Person.new
-ada.walk
 ada.talk
+ada.walk
