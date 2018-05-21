ca04730d022ec86bf52728014588ca400966c6f6
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-05-21 18:52:47 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,20 @@
+class Dog
+ # def initialize(breed, age, name)
+ #   @breed = breed
+ #   @age = age
+ #   @name = name
+ # end
+
+ def bark
+   puts "Woof!"
+ end
+
+ def sit
+   puts "The Dog is sitting"
+ end
+
+end
+
+fido = Dog.new
+snoopy = Dog.new
+lassie = Dog.new
