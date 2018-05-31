0873eb7bece36c4e54f07c5bbf1384e805633487
Joan' Araujo <wisdombthegame@gmail.com>
2018-05-31 00:40:53 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,14 +1,12 @@
 class Dog 
-  def bark 
-   puts "Woof!"
+def bark
+  puts "Woof!"
   end
-  def sit  
-    puts "The Dog is sitting"
-  end 
+def sit 
+  puts "The Dog is sitting"
+  end
 end
 
- fido = Dog.new 
-
- fido.bark 
- fido.sit  
-  
+fido = Dog.new 
+fido.bark 
+fido.sit 
--- lib/person.rb
+++ lib/person.rb
@@ -1,9 +1,16 @@
-class Person
-  def talk
-    puts "Hello World!"
-  end
-  def walk
-    puts "The Person is walking"
-  end
+class Person 
+
+def talk
+  puts "Hello World!"
 end
 
+def walk
+  puts "The Person is walking"
+  end
+end
+  
+  
+ada = Person.new 
+
+ada.talk
+ada.walk 
