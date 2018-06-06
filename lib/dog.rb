872a1b51f5089567afdfe13e229b1f0f550294ad
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2018-06-06 17:56:31 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,22 @@
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+ 
+fido = Dog.new
+fido.bark #> "Woof!"
+ 
+snoopy = Dog.new
+snoopy.bark #> "Woof!"
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+ 
+ 
+ 
+ 
+
