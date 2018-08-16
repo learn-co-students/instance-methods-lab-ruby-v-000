ff040c1223d9917747510a02be5f406b85b3c1f9
Liebe Brand <liebebrand@gmail.com>
2018-08-16 18:39:44 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,16 @@
+
+# 1. Define Dog in lib/dog.rb
+
+# 2. Define #bark in Dog
+
+# 3. Define #sit in Dog
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+
+  def sit
+    puts "The Dog is sitting"
+  end
+end
---
+++ lib/person.rb
@@ -0,0 +1,12 @@
+
+# 4. Define a Person in lib/person.rb
+
+class Person
+  def talk
+    puts "Hello World!"
+  end
+
+  def walk
+    puts "The Person is walking"
+  end
+end
