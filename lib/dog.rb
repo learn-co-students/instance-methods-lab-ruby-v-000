67b9f936bf0acd300783d5268958a9ed57dc84d5
Stef Claus <stef.claus@gmail.com>
2018-08-27 01:29:10 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,8 @@
+class Dog 
+  def bark 
+    puts "Woof!"
+  end #method 
+  def sit 
+    puts "The Dog is sitting"
+  end #ends method 
+end #ends class
---
+++ lib/person.rb
@@ -0,0 +1,8 @@
+class Person 
+  def talk 
+    puts "Hello World!"
+  end #ends method 
+  def walk 
+    puts "The Person is walking"
+  end #ends method 
+end #ends class 
