2066aba3dc03d27ccdfd721a87b4da4b7ddd6568
David Anderson <matandersonlv@hotmail.com>
2018-09-19 18:28:49 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,13 @@
+# Creating a new class called "Dog"
+class = Dog.new
+  # Creating a method within my Dog class called "bark"
+  def bark
+    # defining what that method does
+    puts "Woof!"
+  end
+  # Creating a method within my Dog class called "sit"
+  def sit
+    # Defining what that method does
+    puts "The Dog is sitting"
+  end
+end
