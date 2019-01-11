ffb185642558f65881cd3dfde9d3524af42cfb33
Brandon Brasson <rocksong622@gmail.com>
2019-01-11 21:44:11 +0000

    Done.

--- spec/instance_methods_spec.rb
+++ spec/instance_methods_spec.rb
@@ -1,16 +1,16 @@
-puts describe 'Dog' do
+ describe 'Dog' do
   it 'is defined within lib/dog.rb' do
     expect(defined?(Dog)).to be_truthy
     expect(Dog).to be_a(Class)
   end
 
-  puts describe '#bark' do
+ describe '#bark' do
     it 'responds to the #bark instance method' do
       fido = Dog.new
       expect(fido).to respond_to(:bark)
     end
 
-  puts  it 'puts "Woof!"' do
+  it 'puts "Woof!"' do
       expect($stdout).to receive(:puts).with("Woof!")
 
       fido = Dog.new
@@ -18,13 +18,13 @@ puts describe 'Dog' do
     end
   end
 
-  puts describe '#sit' do
+   describe '#sit' do
     it 'responds to the #sit instance method' do
       fido = Dog.new
       expect(fido).to respond_to(:sit)
     end
 
-    puts it 'puts "The Dog is sitting"' do
+     it 'puts "The Dog is sitting"' do
       expect($stdout).to receive(:puts).with("The Dog is sitting")
 
       fido = Dog.new
@@ -33,19 +33,19 @@ puts describe 'Dog' do
   end
 end
 
-puts describe 'Person' do
+ describe 'Person' do
   it 'is defined within lib/person.rb' do
     expect(defined?(Person)).to be_truthy
     expect(Person).to be_a(Class)
   end
 
-  puts describe '#talk' do
+   describe '#talk' do
     it 'responds to the #talk instance method' do
       ada = Person.new
       expect(ada).to respond_to(:talk)
     end
 
-    puts it 'puts "Hello World!"' do
+     it 'puts "Hello World!"' do
       expect($stdout).to receive(:puts).with("Hello World!")
 
       ada = Person.new
@@ -53,13 +53,13 @@ puts describe 'Person' do
     end
   end
 
-puts  describe '#walk' do
+ describe '#walk' do
     it 'responds to the #walk instance method' do
       ada = Person.new
       expect(ada).to respond_to(:walk)
     end
 
-    puts it 'puts "The Person is walking"' do
+     it 'puts "The Person is walking"' do
       expect($stdout).to receive(:puts).with("The Person is walking")
 
       ada = Person.new
