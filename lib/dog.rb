2458ef3cd57d58b96fd7b4082dcfb5ec476ffef5
Julie  McManus <julieannesays@gmail.com>
2020-01-30 19:56:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,21 +1,9 @@
 class Dog
-	
-	def bark 
+	def bark
 		puts "Woof!"
 	end
 
-	def sit 
+	def sit
 		puts "The Dog is sitting"
-	end
-end
-
-class Person 
-
-	def talk 
-		puts "Hello World!"
-	end
-
-	def walk 
-		puts "The Person is walking"
 	end
 end
